9f989cef5bb93f3006c55ad60fae3a3f0053ce20
omkargarde <42486951+omkargarde@users.noreply.github.com>
2023-06-16 22:00:03 +0530

    added dropdown

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,69 +1,78 @@
-import React from 'react';
-import {useState,useEffect} from "react"
-import UserData from './userData/UserData';
-// import Select from "react-dropdown-select";
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
+import UserData from "./userData/UserData";
 
-
-function Hero()
-{
-    const API = "https://jsonplaceholder.typicode.com/users";
+function Hero() {
+  const API = "https://jsonplaceholder.typicode.com/users";
   const [users, setUsers] = useState([]);
-
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
   const fetchUsers = async (url) => {
     try {
-        const res = await fetch(url);
-        const data = await res.json();
-        if (data.length > 0) {
-            setUsers(data);
-        }
-        console.log(data);
+      const res = await fetch(url);
+      const data = await res.json();
+      if (data.length > 0) {
+        setUsers(data);
+      }
+      console.log(data);
     } catch (e) {
-        console.error(e)
+      console.error(e);
     }
-}
+  };
 
-useEffect(() => {
-  fetchUsers(API);
-}, [])
+  useEffect(() => {
+    fetchUsers(API);
+  }, []);
 
-// const options = [
-//     { 
-//       value: 1,
-//       label: "Leanne Graham"
-//     },
-//     {
-//       value:  2,
-//       label: "Ervin Howell"
-//     }
-//   ];
+  // const options = [
+  //     {
+  //       value: 1,
+  //       label: "Leanne Graham"
+  //     },
+  //     {
+  //       value:  2,
+  //       label: "Ervin Howell"
+  //     }
+  //   ];
 
-    return (
-        <div>
-        <section className='min-h-screen flex flex-col justify-start items-center p-5 text-center'>
-            <h2 className='text-4xl text-black-600 uppercase '>Trending</h2>
-            <h3 className='py-3 text-2xl'>See what the GitHub community is most excited about today.</h3>
-            <table>
-            <thead>
+  return (
+    <div>
+      <section className="min-h-screen flex flex-col justify-start items-center p-5 text-center">
+        <h2 className="text-4xl text-black-600 uppercase ">Trending</h2>
+        <h3 className="py-3 text-2xl">
+          See what the GitHub community is most excited about today.
+        </h3>
+        <table>
+          <thead>
             <tr>
-                <td><button className="px-1 py-0 bg-blue-500 text-white rounded-md">Repository</button></td>
-                <td className="pl-80">Name</td>
-                <td className="pl-80">Email</td>
-                <td className="pl-80">Address</td>
+              <td>
+                <button className="px-1 py-0 bg-blue-500 text-white rounded-md">
+                  Repository
+                </button>
+              </td>
+              <td className="pl-80">
+                <Select options={options} />
+              </td>
+              <td className="pl-80">
+                {" "}
+                <Select options={options} />
+              </td>
+              <td className="pl-80">
+                {" "}
+                <Select options={options} />
+              </td>
             </tr>
-            </thead>
-            <tbody>
-            <UserData users={users}/>
-            </tbody>
-            </table>
-        </section>
-        
-        </div>
-      )
-    
+          </thead>
+          <tbody>
+            <UserData users={users} />
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
 }
 
-    
-
-  
-
-export default Hero
+export default Hero;
