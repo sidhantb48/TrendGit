42dcbf4459c07cf5eedc0c1914ad4362ae75e085
sidhantb48 <116626112+sidhantb48@users.noreply.github.com>
2023-06-17 15:24:44 +0530

    commit
    updated

--- src/components/userData/UserData.jsx
+++ src/components/userData/UserData.jsx
@@ -1,27 +1,23 @@
-const UserData = ({users}) => {
-    console.log(users)
-    return (
-        <>
-        
-            {
-                users.map((curUser) => {
-                    const {id, name, email} = curUser;
-                    const {street, city, zipcode} = curUser.address;
-                    
+const UserData = ({ users }) => {
+  console.log(users);
+  return (
+    <>
+      {users.map((curUser) => {
+        const { id, name, email } = curUser;
+        const { street, city, zipcode } = curUser.address;
 
-                    return (
-                        <tr key={id}>
-                            <td>{id}</td>
-                            <td>{name}</td>
-                            <td>{email}</td>
-                            <td>{street}, {city}, {" "}, {zipcode}</td>
-                        </tr>
-                        
-                    )
-                })
-
-            }
-        </>
-    )
-}
+        return (
+          <tr key={id}>
+            <td>📃{id}</td>
+            <td>{name}</td>
+            <td>{email}</td>
+            <td>
+              {street}, {city}, , {zipcode}
+            </td>
+          </tr>
+        );
+      })}
+    </>
+  );
+};
 export default UserData;
